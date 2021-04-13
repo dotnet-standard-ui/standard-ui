af22a87fcd44881000afc1a1f2fcef95fca888fe
Bret Johnson <bret.johnson@microsoft.com>
2021-04-12 23:58:34 -0400

    Remove TODO

--- src/StandardUI.Base/Media/PathFigure.cs
+++ src/StandardUI.Base/Media/PathFigure.cs
@@ -7,7 +7,6 @@ namespace Microsoft.StandardUI.Media
     {
         public IReadOnlyList<PathSegment> Segments { get; init; } = Array.Empty<PathSegment>();
 
-        // TODO: Supply default point
         public Point StartPoint { get; init; }
 
         public bool IsClosed { get; init; } = false;
